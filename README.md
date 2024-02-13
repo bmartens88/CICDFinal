689029eec51f67b863c2daba373115bc1addb152
Bas Martens <bas.martens.1988@outlook.co>
2024-02-13 15:21:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CICDFinal
+A test repository in order to test a well-defined and structured CI/CD setup with GitHub Actions.
---
+++ test/MyLibrary.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace MyLibrary.Unit.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
